7778df7fdf7cce77a3ac49cc342343016ceae21c
carlos20byte <tellocarlosalan@gmail.com>
2025-07-05 12:31:02 -0600

    Create README.py

---
+++ README.py
@@ -0,0 +1,109 @@
+# IGinjector
+
+[Español🇪🇸](https://github.com/RIP-Network/IGinjector/blob/main/README.md) | [English🇬🇧](https://github.com/RIP-Network/IGinjector/blob/main/modules/README-EN.md)
+
+
+Herramienta de OSINT profundo para la red social instagram, esta herramienta esta creada con python para sacar la maxima infornacion de un usuario de Instagram de manera sencilla y rapida.
+
+La herramienta esta limitada y por ahora solo funciona la segunda opcion. Estos errores se solucionaran en un futuro para que la herramienta funcione correctamente sin limites.
+
+Es recomendable no usar VPN y proxys no mejorara o arreglará el escaneo, tener mala conexion a internet puede influir en el escaneo haciendo que pueda llegar a dar algun error no deseado.
+
+<p align="center"><img width="120px" alt="Version" src="https://img.shields.io/badge/IGinjector-Instagram_OSINT-red"/></p>
+
+<p align="center"><img width="120px" alt="Version" src="https://img.shields.io/badge/version-8.0-red.svg?style=for-the-badge"/></p>
+
+<p align="center"> <img width="400" height="300" src="https://github.com/RIP-Network/IGinjector/blob/main/fotos/logo.png"> </p>
+
+# Instalacion
+```bash
+
+$ apt-get update
+
+$ apt-get upgrade
+
+$ git clone https://github.com/RIP-Network/IGinjector
+
+$ cd IGinjector
+
+$ bash install.sh
+
+$ python3 IGinjector.py
+```
+
+# Bypass Cuenta Privada
+
+Esta herramienta es capaz de saltarse la cuenta si es privada y ver sus publicaciones tiene una opción en su menú para poder llegar a conseguirlo, una vez ejecutada esa funcion tendras que esperar 12h y luego volver a hacer el escaneo con la opcion 2 y en la victima poner la cuenta privada, si todo sale bien aparecera la info.
+
+Si la cuenta está inactiva y abandonada el bypass no funcionara, la cuenta tiene que estar activar y que la persona víctima la use de vez en cuando, una vez se ejecute la opción deberemos esperar 12h para los resultados y volveremos a ejecutar el script, se recomienda esperar el tiempo las prisas pueden hacer que el bypass no surga.
+
+<p align="center"> <img width="400" height="300" src="https://github.com/RIP-Network/IGinjector/blob/main/fotos/foto4.png"> </p>
+
+# Requisitos
+```bash
+
+[*] Chrome
+
+[*] ChromeDriver
+
+[*] Selenium
+
+[*] Python 3.8
+```
+# Sistemas testeados
+```bash
+
+[*] Parrot OS
+
+[*] Kali Linux 
+
+[*] Ubuntu 
+```
+# Informacion extraida
+```bash
+
+[*] Foto de perfil
+
+[*] Nombre de Usuario 
+
+[*] Nickname
+
+[*] Publicaciones 
+
+[*] Seguidores 
+
+[*] Seguidos 
+
+[*] Descripcion 
+
+[*] Enlaces 
+
+[*] Creacion de la cuenta
+
+[*] Email o Numero
+
+[*] Ubicación de las publicaciones
+
+[*] Primeros 5 seguidores 
+
+[*] Ultimos 5 seguidores
+```
+# Vídeo
+
+[![Demo](https://images.unsplash.com/photo-1611162616475-46b635cb6868?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8eW91dHViZSUyMHRodW1ibmFpbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1200&q=60)](https://youtu.be/1bTwZ0IuLJY?si=rIK4NMC1rbBE-LAC)
+
+# Reportar error
+
+Si crees que has encontrado algún fallo puedes reportarlo [AQUI](https://github.com/RIP-Network/IGinjector/issues/new).
+
+[![Logo de GitHub](https://cdn.wallpapersafari.com/34/82/YRzXPk.jpeg)](https://github.com/)
+
+# Redes Sociales
+
+* YT : https://youtube.com/@RIPNetwork
+  
+* YT : https://youtube.com/@RIP-Network
+  
+* IG : https://instagram.com/ripnetworkyt
+  
+* GH : https://github.com/RIP-Network 
